0f9ff2f4df67c1fbfa3024716c9e13cbb94683df
hiratake26to <hiratake26to@gmail.com>
2018-02-06 15:50:57 +0900

    update

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   pack_path: "../../tgim-packager/tgim-pack",
-  gen_path: "../../tgim-generator/tgim-ns3",
+  gen_path: "../../tgim-generator/tgim-generator",
   gen_flags: ['--template-path', '/home/ubuntu-zioi/local/src/tgim-generator/resource/ns3template-cxx.json']
 }
